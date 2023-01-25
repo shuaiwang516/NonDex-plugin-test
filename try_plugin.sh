8db8cd78d624eb250329941de6e90e2660074c8d
Xinyu Wu <xinyuwu264@gmail.com>
2023-01-25 12:33:08 -0600

    generate flaky.csv

--- try_plugin.sh
+++ try_plugin.sh
@@ -6,6 +6,7 @@ path=$(pwd)
 echo $path
 result_file=${path}/result.csv
 
+
 function download_compile() {
     cd $path
     user=$(dirname $1)
@@ -41,14 +42,15 @@ function download_compile() {
 		projects=$(./gradlew projects | grep Project | cut -f3 -d" " | tr -d "':")
     # ----------------------------------------------------adding the plugin-----------------------------------------------------------#
         echo try to add the plugin
-		grep "classpath 'edu.illinois.nondex:edu.illinois.nondex.gradle.plugin:2.1.1'" build.gradle
+		buildFile=$(./gradlew properties | grep buildFile | awk '{print $2}')
+		grep "classpath 'edu.illinois.nondex:edu.illinois.nondex.gradle.plugin:2.1.1'" ${buildFile}
 		if [ $? != 0 ]; then
 			./gradlew projects | grep "No sub-projects"
 			sub=$?	# sub=0 if no subprojects; sub=1 if there are subprojects
 			if [ $sub == 0 ]; then # no subprojects
-				echo -e "\napply plugin: 'edu.illinois.nondex'" >> build.gradle
+				echo -e "\napply plugin: 'edu.illinois.nondex'" >> ${buildFile}
 			else
-				echo -e "\nsubprojects {\n    apply plugin: 'edu.illinois.nondex'\n}" >> build.gradle
+				echo -e "\nsubprojects {\n    apply plugin: 'edu.illinois.nondex'\n}" >> ${buildFile}
 			fi
         	
         	echo "buildscript {
@@ -60,14 +62,15 @@ function download_compile() {
     classpath 'edu.illinois.nondex:edu.illinois.nondex.gradle.plugin:2.1.1'
   }
 }
-$(cat build.gradle)" > build.gradle
+$(cat ${buildFile})" > ${buildFile}
         fi
 
 		# change test closures to tasks.withType(Test)
-		sed -i 's/test /tasks.withType(Test) /' build.gradle
+		sed -i 's/^\( \|\t\)*test /tasks.withType(Test) /' ${buildFile}
 		if [[ $sub != 0 ]]; then # have subprojects
-			for p in ${projects}; do 
-				sed -i 's/^\( \|\t\)*test /tasks.withType(Test) /' $p/build.gradle
+			for p in ${projects}; do
+				subBuildFile=$(./gradlew :$p:properties | grep buildFile | awk '{print $2}')
+				sed -i 's/^\( \|\t\)*test /tasks.withType(Test) /' ${subBuildFile}
 			done
 		fi
         # -----------------------------------------------------run nondexTest--------------------------------------------------------------#
@@ -78,9 +81,16 @@ $(cat build.gradle)" > build.gradle
 			./gradlew nondexTest --nondexRuns=10 1> nondex.log 2> nondex-err.log
 			if ( grep "NonDex SUMMARY:" nondex.log ); then # if nondexTest is actually executed
 				flaky_tests=$(sed -n -e '/Across all seeds:/,/Test results can be found at: / p' nondex.log | sed -e '1d;$d' | cut -f1 -d' ' --complement | tr '\n' ';' | sed 's/.$//') # no new line char at the end
-				if [[ $flaky_tests = '' ]]; then flaky_tests="no flaky tests"; fi
+				if [[ $flaky_tests = '' ]]; then 
+					flaky_tests="no flaky tests"
+				else
+					sha=$(git rev-parse HEAD)
+					sed -n -e '/Across all seeds:/,/Test results can be found at: / p' nondex.log | sed -e '1d;$d' | cut -f1 -d' ' --complement | while read line
+					do echo "https://github.com/$1,${sha},.,${line}" >> ${path}/flaky.csv
+					done	
+				fi
 			else
-				echo "error or no test in the project"
+				echo "error or no test in the project $1"
 				if ( grep "BUILD SUCCESSFUL" nondex.log ); then flaky_tests="no test"
 				else flaky_tests="error"; cp nondex-err.log ${path}/error_log/nondex-${user}-${repo}.log; fi
 			fi
@@ -90,9 +100,15 @@ $(cat build.gradle)" > build.gradle
 				./gradlew :$p:nondexTest  --nondexRuns=10 1> nondex:$p.log 2> nondex-err:$p.log
 				if ( grep "NonDex SUMMARY:" nondex:$p.log ); then # if nondexTest is actually executed
 					flaky_tests=$(sed -n -e '/Across all seeds:/,/Test results can be found at: / p' nondex:$p.log | sed -e '1d;$d' | cut -f1 -d' ' --complement | tr '\n' ';' | sed 's/.$//') # no new line char at the end
-					if [[ $flaky_tests = '' ]]; then flaky_tests="no flaky tests"; fi
+					if [[ $flaky_tests = '' ]]; then flaky_tests="no flaky tests"
+					else 
+						sha=$(git rev-parse HEAD)
+						sed -n -e '/Across all seeds:/,/Test results can be found at: / p' nondex:$p.log | sed -e '1d;$d' | cut -f1 -d' ' --complement | while read line
+						do echo "https://github.com/$1,${sha},$p,${line}" >> ${path}/flaky.csv
+						done
+					fi
 				else
-					echo "error or no test in the project"
+					echo "error or no test in the project $1:$p"
 					if ( grep "BUILD SUCCESSFUL" nondex:$p.log ); then flaky_tests="no test"
 					else flaky_tests="error"; cp nondex-err:$p.log ${path}/error_log/nondex-${user}-${repo}:$p.log; fi
 				fi
@@ -103,7 +119,7 @@ $(cat build.gradle)" > build.gradle
     # ----------------------------------------build fail------------------------------------------------ #
         build=F
         flaky_tests="N/A"
-        echo project has error
+        echo "project $1 has error"
         cp build-err.log ${path}/error_log/build-${user}-${repo}.log
 		echo -e "$1,${build},${ver},${flaky_tests}" >> ${path}/result.csv
     fi
@@ -111,9 +127,12 @@ $(cat build.gradle)" > build.gradle
 }
 
 touch result.csv
+touch flaky.csv
 echo "project name,compile,gradle version,flaky tests" > result.csv
+echo "Project URL,SHA Detected,Subproject Name,Fully-Qualified Test Name (packageName.ClassName.methodName)" > flaky.csv
 mkdir error_log
 for f in $(cat $1); do
     echo ========== trying to dowload $f
     download_compile $f
 done
+
